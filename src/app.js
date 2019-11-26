067ed264a59dd6586aa0d85e05cb9daed83fafd9
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-11-26 17:03:16 -0300

    Created connection with MongoDB

--- src/app.js
+++ src/app.js
@@ -2,8 +2,18 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connects to DB
+mongoose.connect(
+  'mongodb+srv://pmarcosfelipe:s1f65v9200@cluster0-b4zlk.azure.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }
+);
 
 const indexRoute = require('./routes/index');
 const productRoute = require('./routes/product');
