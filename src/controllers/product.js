8bc4f378484adc18ab25bead6e9cdd4f79bda670
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-11-26 22:54:07 -0300

    Implemented get all products, getBySlug, getByID and getByTag methods

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -3,6 +3,52 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = (req, res, next) => {
+  Product.find({ active: true }, 'title price slug')
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+};
+
+exports.getBySlug = (req, res, next) => {
+  Product.findOne(
+    { slug: req.params.slug, active: true },
+    'title description price slug tags'
+  )
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+};
+
+exports.getByID = (req, res, next) => {
+  Product.findById(req.params.id)
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+};
+
+exports.getByTag = (req, res, next) => {
+  Product.find(
+    { tags: req.params.tag, active: true },
+    'title description price slug tags'
+  )
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+};
+
 exports.post = (req, res, next) => {
   var product = new Product(req.body);
 
--- src/routes/product.js
+++ src/routes/product.js
@@ -4,6 +4,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/product');
 
+router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
+router.get('/product/:id', controller.getByID);
+router.get('/tags/:tag', controller.getByTag);
+router.get('/', controller.get);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
