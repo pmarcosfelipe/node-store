1fedb798065a214cc40786b842653023f74904ec
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-11-27 01:53:25 -0300

    Added validator on post product

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/validator');
 
 exports.get = (req, res, next) => {
   Product.find({ active: true }, 'title price slug')
@@ -50,6 +51,31 @@ exports.getByTag = (req, res, next) => {
 };
 
 exports.post = (req, res, next) => {
+  let contract = new ValidationContract();
+  contract.hasMinLen(
+    req.body.title,
+    3,
+    'O título deve conter pelo menos 3 caracteres!'
+  );
+  contract.hasMinLen(
+    req.body.slug,
+    3,
+    'O slug deve conter pelo menos 3 caracteres!'
+  );
+  contract.hasMinLen(
+    req.body.description,
+    3,
+    'O description deve conter pelo menos 3 caracteres!'
+  );
+
+  if (!contract.isValid()) {
+    res
+      .status(400)
+      .send(contract.errors())
+      .end();
+    return;
+  }
+
   var product = new Product(req.body);
 
   product
