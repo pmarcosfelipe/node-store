4350459e4f5c0d62f82ad71c65bb4a45994bf5be
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-11-28 13:03:58 -0300

    Added Order Model

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ mongoose.connect(process.env.DATABASE, {
 
 const Product = require('./models/product');
 const Customer = require('./models/customer');
+const Order = require('./models/order');
 
 const indexRoute = require('./routes/index');
 const productRoute = require('./routes/product');
