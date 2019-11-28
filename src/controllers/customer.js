7bee4c852053a38709f7559943ec4b84f350ec54
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-11-28 14:30:21 -0300

    Added MD5 to customer password

--- src/controllers/customer.js
+++ src/controllers/customer.js
@@ -2,6 +2,7 @@
 
 const ValidationContract = require('../validators/validator');
 const customerRepository = require('../repositories/customer');
+const md5 = require('md5');
 
 exports.post = async (req, res, next) => {
   let contract = new ValidationContract();
@@ -26,7 +27,11 @@ exports.post = async (req, res, next) => {
   }
 
   try {
-    await customerRepository.create(req.body);
+    await customerRepository.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: md5(req.body.password + global.SALT_KEY)
+    });
     res.status(201).send({ message: 'Cliente cadastrado com sucesso!' });
   } catch (error) {
     res.status(500).send({ message: 'Falha ao processar sua requisição' });
