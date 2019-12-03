538bb53c7e53f02f029bdf7770d65737160aafc6
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-12-03 16:55:25 -0300

    Enabled CORS

--- src/app.js
+++ src/app.js
@@ -24,6 +24,16 @@ const orderRoute = require('./routes/order');
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, x-access-token'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 app.use('/', indexRoute);
 app.use('/products', productRoute);
 app.use('/customers', customerRoute);
